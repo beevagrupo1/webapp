235cbcefc425fbcc3a57254f8a493260bf7de7ea
sgavmp <sga.vmp@gmail.com>
2015-07-01 12:11:38 +0200

    Añadido login con Github y Twitter. Modificado estilos de botones sociales.

--- Druzi/templatetags/gravatar.py
+++ Druzi/templatetags/gravatar.py
@@ -34,7 +34,7 @@ class GravatarUrlNode(template.Node):
         size = 40
 
         gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
-        gravatar_url += urllib.urlencode({'d':default, 's':str(size)})
+        gravatar_url += urllib.urlencode({'s':str(size)})
 
         return gravatar_url
 
