4c7baf4793116510d708ddfbe35614840d3e116e
imvitaliy <vitaliyb87@gmail.com>
2015-07-13 10:20:27 +0200

    Stars ;)

--- Druzi/admin.py
+++ Druzi/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Activity,Enrollment
+from .models import Activity,Enrollment, Review
 
 # Register your models here.
 admin.site.register(Activity)
 admin.site.register(Enrollment)
+admin.site.register(Review)
--- Druzi/models.py
+++ Druzi/models.py
@@ -2,7 +2,16 @@ from django.core.exceptions import ValidationError
 from django.db import models
 from geoposition.fields import GeopositionField
 from django.contrib.auth.models import User
+from django.db.models import Avg
 
+RATING_CHOICES = (
+    (0, 'None'),
+    (1, '*'),
+    (2, '**'),
+    (3, '***'),
+    (4, '****'),
+    (5, '*****'),
+    )
 # Create your models here.
 class Activity(models.Model):
     title = models.CharField(max_length=150, blank=False)
@@ -18,8 +27,13 @@ class Activity(models.Model):
     limit_participants = models.IntegerField(null=True, default=0)
     participants = models.ManyToManyField(User, default=0, through="Enrollment", related_name="participants")
     tags = models.ManyToManyField("Tag", through="TagAppear", related_name="tags")
-
     visit_count = models.IntegerField(default=0)
+    def get_average_rating(self):
+        average = self.review_set.all().aggregate(Avg('rating'))['rating__avg']
+        if average == None:
+            return average
+        else:
+            return int(average)
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, primary_key=True)
@@ -41,4 +55,9 @@ class Enrollment(models.Model):
 
 #Validadores
 
+class Review(models.Model):
+    activity = models.ForeignKey(Activity)
+    user = models.ForeignKey(User)
+    rating = models.IntegerField(choices=RATING_CHOICES, null=True, blank=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
 
