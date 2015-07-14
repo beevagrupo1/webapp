953c88cf6670b41148a1a252cce92d8056afa1a0
sgavmp <sga.vmp@gmail.com>
2015-07-14 08:53:17 +0200

    Cambios de configruacion

--- projectdruzi/settings.py
+++ projectdruzi/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import sys
+import dj_database_url
 from django.conf import global_settings
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -73,8 +74,14 @@ WSGI_APPLICATION = 'projectdruzi.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config(os.getenv('CLEARDB_DATABASE_URL', "mysql://root:root@localhost/druzi?reconnect=true"))
+DATABASES = { 'default' : dj_database_url.config(os.getenv('CLEARDB_DATABASE_URL', "mysql://root:root@localhost/druzi?reconnect=true"))}
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+    }
+}
+
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
