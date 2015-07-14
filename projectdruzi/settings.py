1dafda0b29ef04e5c7d7e90216296262552201d6
sgavmp <sga.vmp@gmail.com>
2015-07-14 14:25:50 +0200

    Modificado setting para permitir despliegue automatico en heroku

--- projectdruzi/settings.py
+++ projectdruzi/settings.py
@@ -14,7 +14,6 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import sys
-import dj_database_url
 from django.conf import global_settings
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
