db46ecbf1a55a78c484f0b6b31f448f55bcaf46e
mlumeras <m.lumeras@gmail.com>
2015-07-21 13:43:04 +0200

    Faltan commits para que funcione el contact_us :)

--- Druzi/views.py
+++ Druzi/views.py
@@ -401,5 +401,23 @@ def profile(request, username, page="1"):
 
         
 def about_us(request, anchor = None):
-
+    errors = []
+    if request.method == 'POST':
+        if not request.POST.get('subject', ''):
+            errors.append('Enter a subject.')
+        if not request.POST.get('message', ''):
+            errors.append('Enter a message.')
+        if request.POST.get('email') and '@' not in request.POST['email']:
+            errors.append('Enter a valid e-mail address.')
+        if not errors:
+          try:
+            send_mail(
+                request.POST['subject'],
+                request.POST['message'],
+                request.POST.get('email', 'support@ruunalbe.com'),
+                ['siteowner@example.com'],
+            )
+            return HttpResponse('Thank you, form has been submitted successfully')
+          except Exception, err: 
+            return HttpResponse(str(err))
     return render(request, 'webapp/about_us.html')      
